a5da915730fee2b2adc4a24b797e6e10f1f7e649
Kristian Mandrup <kmandrup@gmail.com>
2012-04-23 11:28:11 +0200

    added locales generator

---
+++ lib/generators/i18n_docs/locales_generator.rb
@@ -0,0 +1,45 @@
+# for lang in de en it fr; do \
+#     mkdir -p config/locales/$lang; \
+#     touch config/locales/$lang/navigation.yml; \
+#     touch config/locales/$lang/forms.yml; \
+#   done
+module I18nDocs
+  module Generators
+    class LocalesGenerator < Rails::Generators::Base
+      desc "Creates Locale files for I18n-docs"
+
+      argument     :locales,           :type => :array,      :desc => "Locale codes"
+
+      source_root File.dirname(__FILE__) + '/templates'
+
+      def main_flow
+        locales.each do |locale|
+        	self.locale = locale
+          exec_template
+        end
+      end
+
+      protected
+
+      def files_src_path
+      	File.join Rails.root, 'config', 'translations.yml'
+      end
+
+      def yaml
+      	YAML::load File.open(files_src_path)
+      end
+
+      def files
+      	yaml['files'].keys
+      end
+
+      attr_accessor :locale
+
+      def exec_template
+      	files.each do |file|      	
+        	template "locale.erb", "config/locales/#{locale}/#{file}"
+        end
+      end
+    end
+  end
+end
