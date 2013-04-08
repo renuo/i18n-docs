b57331fcd48d15b7ffd306257ee9de1dead384b0
Esteban Pastorino <ejpastorino@gmail.com>
2013-04-08 20:22:53 -0300

    fix utf-8 related error

--- lib/localch_i18n/translations.rb
+++ lib/localch_i18n/translations.rb
@@ -1,4 +1,4 @@
-#
+# encoding: utf-8
 # Order of method calls
 #  download_files
 #  store_translations
@@ -6,29 +6,29 @@
 #
 module LocalchI18n
   class Translations
-    
+
     attr_accessor :locales, :tmp_folder, :config_file, :csv_files
-    
+
     def initialize(config_file = nil, tmp_folder = nil)
-      @config_file = config_file 
+      @config_file = config_file
       @tmp_folder  = tmp_folder
-      
+
       @csv_files = {}
-      
+
       load_config
       load_locales
     end
-    
+
     def load_locales
       @locales = []
       @locales = I18n.available_locales if defined?(I18n)
     end
-    
+
     def load_config
       @settings = {}
       @settings = YAML.load_file(config_file) if File.exists?(config_file)
     end
-    
+
     def download_files
       files = @settings['files']
       files.each do |target_file, url|
@@ -41,31 +41,32 @@ module LocalchI18n
         @csv_files[target_file] = tmp_file
       end
     end
-    
+
     def store_translations
       @csv_files.each do |target_file, csv_file|
         converter = CsvToYaml.new(csv_file, target_file, @locales)
         converter.process
         converter.write_files
       end
-      
+
       @csv_files
     end
-    
+
     def clean_up
       # remove all tmp files
       @csv_files.each do |target_file, csv_file|
         File.unlink(csv_file)
       end
     end
-    
+
     def download(url, destination_file)
       puts "Download '#{url}' to '#{destination_file}'"
-      File.open(destination_file, 'w') do |dst|
-        dst.write(open(url).read)
+      File.open(destination_file, 'wb') do |dst|
+        doc = open(url).read
+        dst.write(doc)
       end
     end
-    
+
   end
 end
 
