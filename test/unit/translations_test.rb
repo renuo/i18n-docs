88eced329673a6c42a58ab50b04ba99e544da4f0
Eduard Schaeli <eduard.schaeli@benthoo.ch>
2013-06-10 11:53:56 +0200

    Test for UTF-8 encoding

--- test/unit/translations_test.rb
+++ test/unit/translations_test.rb
@@ -53,6 +53,7 @@ module UnitTests
       assert !File.exists?(expected_file)
       @translations.download_files
       assert File.exists?(expected_file)
+      assert File.open(expected_file).read.encoding.name == 'UTF-8'
     end
 
   end
