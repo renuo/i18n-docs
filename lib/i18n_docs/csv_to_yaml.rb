356f98621793a74574d6f44f8d247fc892826e3a
Eric Winter <ehwinter@gmail.com>
2015-09-17 14:22:09 -0400

    import allows use of yaml aliases with better parsing of single/double quoted elements

--- lib/i18n_docs/csv_to_yaml.rb
+++ lib/i18n_docs/csv_to_yaml.rb
@@ -2,7 +2,7 @@ module I18nDocs
 
   class CsvToYaml
 
-    attr_reader :input_file, :output_file, :locales, :translations
+    attr_reader :input_file, :output_file, :locales, :translations, :aliases
 
     def initialize(input_file, output_file, locales = [])
       @input_file = input_file
@@ -11,8 +11,10 @@ module I18nDocs
 
       # init translation hash
       @translations = {}
+      @aliases = {}
       @locales.each do |locale|
         @translations[locale] = {}
+        @aliases[locale] = {}
       end
     end
 
@@ -26,8 +28,10 @@ module I18nDocs
           output_file_path = "#{locale}_#{@output_file}"
         end
         File.open(output_file_path, 'w') do |file|
-          final_translation_hash = {locale => @translations[locale]}
-          file.puts YAML::dump(final_translation_hash)
+          hashes = []
+          alias_hash(@aliases[locale], hashes)
+          final_hash = {locale => @translations[locale]}
+          file.puts remove_alias_quotes(alias_only_corrected(YAML::dump(final_hash), hashes))
         end
         puts "File '#{@output_file}' for language '#{locale}' written to disc (#{output_file_path})"
       end
@@ -47,12 +51,15 @@ module I18nDocs
       key_elements = key.split('.')
       @locales.each do |locale|
         raise "Locale missing for key #{key}! (locales in app: #{@locales} / locales in file: #{row_hash.keys.to_s})" if !row_hash.has_key?(locale)
-        store_translation(key_elements, locale, row_hash[locale])
+        unless alias_only? row_hash[locale]
+          store_translation(key_elements, locale, row_hash[locale], @translations)
+        else
+          store_translation(key_elements, locale, row_hash[locale], @aliases)
+        end
       end
     end
 
-
-    def store_translation(keys, locale, value)
+    def store_translation(keys, locale, value, hash_store)
       return nil if value.nil?    # we don't store keys that don't have a valid value
       # Google Spreadsheet does not export empty strings and therefore we use '_' as a replacement char.
       value = '' if value == '_'
@@ -60,10 +67,10 @@ module I18nDocs
       keys.each(&:strip!)
       tree = keys[0...-1]
       leaf = keys.last
-      data_hash = tree.inject(@translations[locale]) do |memo, k|
+      data_hash = tree.inject(hash_store[locale]) do |memo, k|
         if memo.is_a? Hash
           if memo.has_key?(k)
-            memo[k]
+              memo[k]
           else
             memo[k] = {}
           end
@@ -73,6 +80,47 @@ module I18nDocs
       end
       data_hash[leaf] = value
     end
+
+    private
+      # Must handle aliases with/without a value differently addressable: &addressable vs name: &name Name
+      def alias_only?(key)
+        true if key && key[0] == '&' && key.split.size == 1
+      end
+
+      # leaf hash having an alias as its value
+      def alias_hash(hash, result)
+        hash.each do |k,v|
+          if v.kind_of? Hash
+            alias_hash v, result
+          else
+            if alias_only?(v)
+              result << {k => v}
+            end
+          end
+        end
+        result
+      end
+
+      def alias_only_corrected(yaml_string, alias_only_hashes)
+        alias_only_hashes.each do |hash|
+          hash.each do |k,v|
+            yaml_string.gsub! "#{k}:\n", "#{k}: #{v}\n"
+          end
+        end
+        yaml_string
+      end
+
+      # an alias is quoted by default,
+      # convert blah: "&alias content" to convert blah: &alias content
+      def remove_alias_quotes(yaml_string)
+        yaml_string.scan(/: \"[\&,\*].*\"$/).each do |alias_quoted|
+          yaml_string.gsub!(alias_quoted, alias_quoted.gsub(/: \"/,": ").gsub(/\"$/,""))
+        end
+        yaml_string.scan(/: \'[\&,\*].*\'$/).each do |alias_quoted|
+          yaml_string.gsub!(alias_quoted, alias_quoted.gsub(/: \'/,": ").gsub(/\'$/,""))
+        end
+        yaml_string
+      end
 
   end
 
