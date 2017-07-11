901c284e1eedcc072d860e8477f5bd66f12a9098
Josua Schmid <josua.schmid@renuo.ch>
2017-07-11 14:19:48 +0200

    Fix rubocop Lint/StringConversionInInterpolation

--- lib/i18n_docs/csv_to_yaml.rb
+++ lib/i18n_docs/csv_to_yaml.rb
@@ -43,7 +43,7 @@ module I18nDocs
 
       key_elements = key.split('.')
       @locales.each do |locale|
-        raise "Locale missing for key #{key}! (locales in app: #{@locales} / locales in file: #{row_hash.keys.to_s})" unless row_hash.key?(locale)
+        raise "Locale missing for key #{key}! (locales in app: #{@locales} / locales in file: #{row_hash.keys})" unless row_hash.key?(locale)
         store_translation(key_elements, locale, row_hash[locale])
       end
     end
